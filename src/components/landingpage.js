189afacccf8de13c95336c1f654261fb2eba0696
Maxwell Dubin <maxdhs@gmail.com>
2019-02-22 15:21:06 -0800

    Update landing page component

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 
 export class LandingPage extends Component {
     render() {
-        return(
-            <div>
-                <h1>
-                    LandingPage
-                </h1>
+        return (
+            <div className="container">
+                <h1 class="well" id="title">Hi, My Name's Max</h1>
+
+                <br />
+                <div class="myBorder">
+                    <i id="myIcon" class="fa fa-smile-o fa-3x"></i>
+
+                    <p id="subHeader">What can I create for you today?</p>
+
+                    <p>
+                        My passion is everything <em><b><strong>web design</strong></b></em>. I have experience in HTML, CSS, JavaScript, and Bootstrap. Please browse below and check out some of my projects. I hope you experience some of the wonder at the emerging technologies of web development as I did during their creations.
+                    </p>
+                </div>
+                <br />
             </div>
+
         );
     }
 }
